8a4fe177bcae58b8fb2e93b2f1cdf6c9b85e0c45
Michael Torres <michaelj.torres@yahoo.com>
2019-03-01 17:48:47 -0500

    Shopping list and Ingredients added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes = [];
-  constructor() { }
+recipes: Recipe[] = [
+  new Recipe('A test', 'Simple Test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+];
+
+
+  constructor() {}
 
   ngOnInit() {
   }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient {
+    constructor (public name: string, public amount: number) {}
+}
+//public name: string;
+//public amount: number;
+         // this.name = name;
+         // this.amount = amount;
+// The constructor with PUBLIC inside does everything that is labeled in green as a shortcut
